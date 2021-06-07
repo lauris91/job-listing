7dc4edc179913f0d0cf7cc4678ee9436fec12fe6
Lauris Zaicevs <lauris.zaicevs@abcsoftware.lv>
2021-06-07 14:54:18 +0300

    update images path

--- src/components/JobContainer.jsx
+++ src/components/JobContainer.jsx
@@ -14,33 +14,36 @@ const JobContainer = ({
   },
   labels,
   onLabelClick,
-}) => (
-  //did not understand what is the criteria to display border-left
-  <div className={`job-container ${id === 1 || id === 2 ? "border-left" : ""}`}>
-    <div className="logo">
-      <img alt="company-logo" src={`.${logo}`} />
-    </div>
-    <div className="job-description">
-      <div className="job-description__company-row">
-        {company}
-        {newJob && <div className="label label__new">New!</div>}
-        {featured && <div className="label label__featured">Featured</div>}
+}) => {
+  return (
+    //did not understand what is the criteria to display border-left
+    <div
+      className={`job-container ${id === 1 || id === 2 ? "border-left" : ""}`}
+    >
+      <div className="logo">
+        <img alt="company-logo" src={`../images/${logo}`} />
       </div>
-      <div className="job-description__job-title">{position}</div>
-      <div className="job-description__info-box">
-        <div className="item">{postedAt}</div>
-        <div className="item">{contract}</div>
-        <div className="item">{location}</div>
-      </div>
-    </div>
-    <div className="labels">
-      {labels.map((label, index) => (
-        <div key={index} className="labels__item" onClick={onLabelClick}>
-          {label}
+      <div className="job-description">
+        <div className="job-description__company-row">
+          {company}
+          {newJob && <div className="label label__new">New!</div>}
+          {featured && <div className="label label__featured">Featured</div>}
         </div>
-      ))}
+        <div className="job-description__job-title">{position}</div>
+        <div className="job-description__info-box">
+          <div className="item">{postedAt}</div>
+          <div className="item">{contract}</div>
+          <div className="item">{location}</div>
+        </div>
+      </div>
+      <div className="labels">
+        {labels.map((label, index) => (
+          <div key={index} className="labels__item" onClick={onLabelClick}>
+            {label}
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default JobContainer;
